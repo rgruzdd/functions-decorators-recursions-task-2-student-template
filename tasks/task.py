1489a60126d8823850932733eed88cd5167d6f43
Gruzin <gruzd.roman@inbox.ru>
2023-03-30 16:52:47 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -4,4 +4,22 @@ def linear_seq(sequence: List[Any]) -> List[Any]:
     """
     Add your code here or call it from here   
     """
-    pass
+    new_numbers = []
+    for i in sequence:
+        try:
+            if i == int(i):
+                new_numbers.append(i)
+        except TypeError:
+            for k in i:
+                try:
+                    if k == int(k):
+                        new_numbers.append(k)
+                except TypeError:
+                    for f in k:
+                        new_numbers.append(f)
+    return new_numbers
+
+sequence = [1,2,3,[4,5, (6,7)]]
+print(linear_seq(sequence))
+
+
